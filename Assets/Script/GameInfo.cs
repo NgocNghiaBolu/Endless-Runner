e7a97e85bae24c04ecba404542488c1e9b3aa225
NgocNghiaBolu <nghia048@gmail.com>
2024-03-02 22:45:17 +0700

    init commit

---
+++ Assets/Script/GameInfo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+static public class GameInfo
+{
+	static public int score = 0;
+	static public int bestScore = 0;
+	static public bool accelerometerSteering = false;
+
+	static public void SaveInformation()
+	{
+		PlayerPrefs.SetInt("bestscore", bestScore);
+	}
+	
+	static public void LoadInformation()
+	{
+		bestScore = PlayerPrefs.GetInt("bestscore");
+	}
+}
